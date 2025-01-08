43b5770ca98427d134d5976241c23a49e2d7bf7d
moniruzzaman01 <moniruzzamanshakib04@gmail.com>
2025-01-09 00:54:13 +0600

    initial setup

---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+export default function Home() {
+  return (
+    <div className=" flex justify-center text-white">
+      <div className=" w-full bg-slate-600 rounded">
+        <div className=" flex justify-center gap-2 ">
+          <button className=" px-6 bg-slate-800 rounded">focus</button>
+          <button>break</button>
+        </div>
+      </div>
+      <div className=" w-full bg-green-300"></div>
+    </div>
+  );
+}
---
+++ components/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function Navbar() {
+  return (
+    <div className=" text-white flex items-end justify-between h-12">
+      <div className=" font-bold tracking-wider py-1">SITENAME</div>
+      <div className=" flex gap-2">
+        <button className=" bg-slate-800 px-3 py-1 rounded">button1</button>
+        <button className=" bg-slate-800 px-3 py-1 rounded">button1</button>
+        <button className=" bg-slate-800 px-3 py-1 rounded">button1</button>
+      </div>
+    </div>
+  );
+}
