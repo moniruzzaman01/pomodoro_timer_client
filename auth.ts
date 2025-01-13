cae84600fcaffd91bf56e3222c32775d06715eed
moniruzzaman01 <moniruzzamanshakib04@gmail.com>
2025-01-13 13:58:13 +0600

    auth.ts file edited, secret added after provider

--- auth.ts
+++ auth.ts
@@ -1,6 +1,7 @@
 import NextAuth from "next-auth";
 import Google from "next-auth/providers/google";
-
+console.log("secret", process.env.AUTH_SECRET);
 export const { handlers, signIn, signOut, auth } = NextAuth({
   providers: [Google],
+  secret: process.env.AUTH_SECRET,
 });
